8f4bfa8ed2aa31688072ed14e4bf3c2b6d741666
hugedong69 <pentagonalpentagon@gmail.com>
2017-11-27 18:44:39 +0800

    Add files via upload

--- window.js
+++ window.js
@@ -1,54 +1,54 @@
-const events = require('events');
-const electron = require('electron');
+const events = require('events')
+const electron = require('electron')
 
-const CHANNEL_NAME = 'arborean-apparel';
+const CHANNEL_NAME = 'arborean-apparel'
 
 class Window extends events.EventEmitter {
-  constructor() {
-    super();
-    this.window = null;
-  }
+	constructor() {
+		super()
+		this.window = null
+	}
 
-  show() {
-    if (this.window) {
-      this.window.show();
-      return;
-    }
+	show() {
+		if (this.window) {
+			this.window.show()
+			return
+		}
 
-    this.window = new electron.BrowserWindow({
-      title: 'Arborean Apparel (Alpha)',
-    });
+		this.window = new electron.BrowserWindow({
+			title: 'Arborean Apparel (VERY ALPHA)'
+		})
 
-    const listener = (event, ...args) => {
-      if (event.sender === this.window.webContents) {
-        this.emit(...args);
-      }
-    };
+		const listener = (event, ...args) => {
+			if (event.sender === this.window.webContents) {
+				this.emit(...args)
+			}
+		}
 
-    electron.ipcMain.on(CHANNEL_NAME, listener);
+		electron.ipcMain.on(CHANNEL_NAME, listener)
 
-    this.window.webContents.on('did-finish-load', () => {
-      this.emit('opened');
-    });
+		this.window.webContents.on('did-finish-load', () => {
+			this.emit('opened')
+		})
 
-    this.window.on('closed', () => {
-      this.emit('closed');
-      electron.ipcMain.removeListener(CHANNEL_NAME, listener);
-      this.window = null;
-    });
+		this.window.on('closed', () => {
+			this.emit('closed')
+			electron.ipcMain.removeListener(CHANNEL_NAME, listener)
+			this.window = null
+		})
 
-    this.window.setMenu(null);
-    this.window.loadURL(`file://${__dirname}/www/index.html`);
-  }
+		this.window.setMenu(null)
+		this.window.loadURL(`file://${__dirname}/www/index.html`)
+	}
 
-  send(...args) {
-    if (!this.window) return;
-    this.window.webContents.send(CHANNEL_NAME, ...args);
-  }
+	send(...args) {
+		if (!this.window) return
+		this.window.webContents.send(CHANNEL_NAME, ...args)
+	}
 
-  close() {
-    if (this.window) this.window.close();
-  }
+	close() {
+		if (this.window) this.window.close()
+	}
 }
 
-module.exports = Window;
+module.exports = Window
